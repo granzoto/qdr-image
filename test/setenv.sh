d7beb24f50fb0ecc24a13dc8df6a17f31a95e65d
Fernando Giorgetti <fgiorget@redhat.com>
2020-09-09 18:19:08 -0300

    Initial structure proposal for kubernetes tests

---
+++ test/setenv.sh
@@ -0,0 +1,14 @@
+#
+# Variables defined here can be used by the test suites
+# or either by the client container images that will be
+# executed by them.
+#
+# Note that this is just to reference all environment
+# variables that can be defined. The test suite must
+# set them in the respective pod/jobs when running 
+# against a Kubernetes cluster.
+#
+
+# clients/java/jms-amqp-tests
+QPID_JMS_TRANSACTION_ROUTER_URL="amqp://127.0.0.1:5672"
+QPID_JMS_TRANSACTION_ADDRESS="trx.testQueue"
